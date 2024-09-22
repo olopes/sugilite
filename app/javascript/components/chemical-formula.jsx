00ee30831c6227403c85df463a675b392b58c5b1
olopes <psicover.dev@gmail.com>
2024-09-22 15:00:07 +0100

    update formula to support superscript text using ^ char

--- app/javascript/components/chemical-formula.jsx
+++ app/javascript/components/chemical-formula.jsx
@@ -25,12 +25,21 @@ export default function ChemicalFormula({ formula }) {
 function FormatFormulaPart({ formulaPart }) {
   if (formulaPart === null || formulaPart === undefined) return null;
 
-  return (<li className="formula-part">
-    {
-      formulaPart
-        .trim()
-        .split(/_(\d+)_/)
-        .map((str, idx) => (idx % 2) ? (<sub key={`sub-${idx}`}>{str}</sub>) : (<span key={`span-${idx}`}>{str}</span>))
-    }
-  </li>);
+  const formulaElements = formulaPart
+    .trim()
+    .split(/(\^[^\^]+\^|_[^_]+_)/)
+    .filter(tok => tok.trim() !== "")
+    .map((token, idx) => {
+      if(token.startsWith("_") && token.endsWith("_")) {
+        console.log("Sub Token =>", token);
+        return <sub key={`sub-${idx}`}>{token.substring(1, token.length-1)}</sub>;
+      } else if(token.startsWith("^") && token.endsWith("^")) {
+        console.log("Sup Token =>", token);
+        return <sup key={`sup-${idx}`}>{token.substring(1, token.length-1)}</sup>;
+      } else {
+        console.log("Span Token =>", token);
+        return <span key={`span-${idx}`}>{token}</span>;
+      }
+    });
+  return (<li className="formula-part">{formulaElements}</li>);
 }
