cd49a2e9963ac17a0a51b642bf3a38687543c4bd
olopes <psicover.dev@gmail.com>
2025-11-18 09:11:43 +0000

    upgrade to tailwindcss 4

--- app/javascript/components/gemstone-app.tsx
+++ app/javascript/components/gemstone-app.tsx
@@ -2,7 +2,7 @@ import GemstoneList from "@/components/gemstone-list";
 
 export default function GemstoneApp() {
   return (
-    <main className="h-[calc(100vh-theme(spacing.16))]">
+    <main className="h-[calc(100vh-(--spacing(16)))]">
       <div className="flex flex-1 grow justify-center items-center h-full">
         <GemstoneList />
       </div>
